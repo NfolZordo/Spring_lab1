c1eef874c07fb4c557d515915771aaddbdae6891
NfolZordo <79935610+NfolZordo@users.noreply.github.com>
2023-11-03 14:41:10 +0200

    #1-lab: end

--- src/main/java/com/example/lab2/Product.java
+++
@@ -1,9 +0,0 @@
-package com.example.lab2;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-public interface Product {
-    String getName();
-    double getPrice();
-}
